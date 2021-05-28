dcb3f20da7a79fd3e5670756985b02149dd88657
一陣 <ich_jin@key-web.jp>
2021-05-28 15:01:12 +0900

    接続修正

--- ChatWork.php
+++ ChatWork.php
@@ -1,14 +1,4 @@
 <?php
 echo('ChatWork in');
 exit();
-$dbhost = $_SERVER['start-aws-db-instance.cxulytbuqnlc.ap-northeast-1.rds.amazonaws.com'];
-$dbport = $_SERVER['3036'];
-$dbname = $_SERVER['aws_chat'];
-$charset = 'utf8' ;
-
-$dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-$username = $_SERVER['start_aws_dbuser'];
-$password = $_SERVER['start_aws_db_password'];
-
-$pdo = new PDO($dsn, $username, $password);
 ?>
