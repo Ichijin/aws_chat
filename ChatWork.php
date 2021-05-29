2f01274254bc2ae3b3127b4d86786a9e0b0167ca
Ichijin <57050605+Ichijin@users.noreply.github.com>
2021-05-30 08:58:41 +0900

    Update ChatWork.php

--- ChatWork.php
+++ ChatWork.php
@@ -1,4 +1,22 @@
 <?php
-echo('test echo');
-$dbhost = $_SERVER['start-aws-db-instance.cxulytbuqnlc.ap-northeast-1.rds.amazonaws.com'];
+$link = mysqli_connect('start-aws-db-instance.cxulytbuqnlc.ap-northeast-1.rds.amazonaws.com', 'start_aws_dbuser', 'start_aws_db_password', 'aws_chat');
+
+// 接続状況をチェックします
+if (mysqli_connect_errno()) {
+    die("データベースに接続できません:" . mysqli_connect_error() . "\n");
+} else {
+    echo "データベースの接続に成功しました。\n";
+}
+
+$query = "SELECT * FROM `test`";
+// クエリを実行します。
+if ($result = mysqli_query($link, $query)) {
+    echo "SELECT に成功しました。\n";
+    foreach ($result as $row) {
+        var_dump($row);
+    }
+}
+
+// 接続を閉じます
+mysqli_close($link);
 ?>
