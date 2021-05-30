64379a6b8451193a637dd2e21b004c6c37c57436
Ichirou Jinnou <ich_jin@key-web.jp>
2021-05-30 15:20:53 +0900

    追加

--- index.php
+++ index.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title>チャットボットサンプル</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="ここにサイト説明を入れます">
-<meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
-<link rel="stylesheet" href="css/chatbot.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>チャットボットサンプル</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="ここにサイト説明を入れます">
+    <meta name="keywords" content="キーワード１,キーワード２,キーワード３,キーワード４,キーワード５">
+    <link rel="stylesheet" href="css/chatbot.css">
+    <script
+    src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+    crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -15,35 +19,65 @@
         <div class="chatbot_ico">
         </div>
         <div class="chatbot_bord" style="display: none;">
-            <p class="chat-talk">
-                <span class="talk-icon">
-                <img src="[ファイルのurl]" alt="tartgeticon" width="XX" height="XX"/>
-                </span>
-                <span class="talk-content">[トーク内容を記載]</span>
+            <div class="chatbot_talk" id="chatbot_talk">
+                <p class="chat-talk">
+                    <span class="talk-icon">
+                        <img src="./telephone-operator_female.png" alt="tartgeticon" width="60" height="60"/>
+                    </span>
+                    <span class="talk-content">ご用をお聞かせください。</span>
                 </p>
-                <p class="chat-talk mytalk">
-                <span class="talk-icon">
-                <img src="[ファイルのurl]" alt="myicon" width="XX" height="XX"/>
-                </span>
-                <span class="talk-content">[トーク内容を記載]</span>
-            </p>
+            </div>
+            <div class="cp_box">
+                <div class="cp_iptxt cp_item">
+                    <input type="text" placeholder="お聞きになりたいこと" id="speech">
+                    <i class="fa fa-user fa-lg fa-fw" aria-hidden="true"></i>
+                </div>
+                <div class="cp_item">
+                    <input type="submit" value="送信" class="btn-gradient-radius" />
+                </div>
+            </div>
         </div>
 	</div>
 
+<?php for($i = 0; $i < 50; $i++): ?>
     <p>
         contentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontentscontents
     </p>
     <br>
+<?php endfor; ?>
+
 </body>
 </html>
 
-<script
-    src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossorigin="anonymous"></script>
 <script>
-    $('.chatbot').on('click', function(){
-        $('.chatbot_ico').hide();
-        $('.chatbot_bord').show();
-    });
+$('.chatbot').on('click', function(){
+    $('.chatbot_ico').hide();
+    $('.chatbot_bord').show();
+});
+
+$('.btn-gradient-radius').click(function(){
+    if(!$('#speech').val()) return false;
+
+    // 質問
+    ques = '<p class="chat-talk mytalk">'
+        + '<span class="talk-icon">'
+        + '<img src="./guest-icon.png" alt="myicon" width="60" height="60"/>'
+        + '</span>'
+        + '<span class="talk-content">'+$('#speech').val()+'</span>'
+        + '</p>';
+    $('#speech').val('');
+    $('.chatbot_talk').append(ques);
+
+    // 回答
+    ans = '<p class="chat-talk">'
+        + '<span class="talk-icon">'
+        + '<img src="./telephone-operator_female.png" alt="tartgeticon" width="60" height="60"/>'
+        + '</span>'
+        + '<span class="talk-content">どのようなことでしょうか。</span>'
+        + '</p>';
+    $('.chatbot_talk').append(ans);
+
+    var obj = document.getElementById('chatbot_talk');
+    obj.scrollTop = obj.scrollHeight;
+});
 </script>
